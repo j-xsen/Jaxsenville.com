8343447f32faf03f3b2984c0eafa9f0ae5b27413
jaxsen <jaxsen@jxsen.com>
2025-03-24 23:35:05 -0400

    debug

--- src/pages/(single)/blahg/@id/+guard.js
+++ src/pages/(single)/blahg/@id/+guard.js
@@ -5,8 +5,10 @@ import { supabase } from "../../../../utils/supabase"
 
 const guard = async (pageContext) => {
     if (pageContext.routeParams.id) {
+        console.log("id " + pageContext.routeParams.id)
         let url = await supabase.from('Blahg').select('url').eq('ID', pageContext.routeParams.id)
         if ( url.data && url.data[0]) {
+            console.log("url " + url.data[0].url)
             if (!pageContext.routeParams.glahb) {
                 throw redirect('/blahg/' + pageContext.routeParams.id + '/' + url.data[0].url, 301)
             } else if (pageContext.routeParams.glahb !== url.data[0].url) {
