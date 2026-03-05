8a359a12097104499b3dfa3e1391a1005347550a
Jaxsen Honeycutt <j0hone01@louisville.edu>
2026-03-05 13:42:55 -0500

    fix single page header not being clickable

--- src/pages/(single)/+Layout.tsx
+++ src/pages/(single)/+Layout.tsx
@@ -1,12 +1,11 @@
 import {usePageContext} from "vike-react/usePageContext";
-
-export {Layout}
-
 import React from 'react'
 import PageTitleButton from '../../components/PageTitleButton'
 
 import {useData} from "vike-react/useData"
 import type {Data} from './+data'
+
+export {Layout}
 
 function Layout({children}: { children: React.ReactNode }) {
     const data = useData<Data>()
@@ -14,16 +13,27 @@ function Layout({children}: { children: React.ReactNode }) {
     return (
         <>
             <nav role={"navigation"}>
-                <ul>
+                <ul style={{
+                    position: "relative",
+                    zIndex: 20,
+                    display: "flex",
+                    gap: "1rem",
+                    alignItems: "center",
+                    paddingLeft: "1rem",
+                    paddingRight: "1rem",
+                    margin: "0",
+                    justifyContent: "space-between",
+                }}>
                     <PageTitleButton text={data.path}/>
-                {pageContext.urlParsed.pathname==="/art" && (
-                    <li className="instagram-link-container" onMouseDown={()=>window.open("https://www.instagram.com/j_xsen/", "_blank")}>
-                        <a href="https://www.instagram.com/j_xsen/" target="_blank" className="instagram-link">
-                            <img src="/icon/instagram.svg" className="icon" alt="Instagram logo"/>
-                            @j_xsen
-                        </a>
-                    </li>
-                )}
+                    {pageContext.urlParsed.pathname === "/art" && (
+                        <li className="instagram-link-container"
+                            onMouseDown={() => window.open("https://www.instagram.com/j_xsen/", "_blank")}>
+                            <a href="https://www.instagram.com/j_xsen/" target="_blank" className="instagram-link">
+                                <img src="/icon/instagram.svg" className="icon" alt="Instagram logo"/>
+                                @j_xsen
+                            </a>
+                        </li>
+                    )}
                 </ul>
             </nav>
             <div className="Gallery" tabIndex={-1}>
